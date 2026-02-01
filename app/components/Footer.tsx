16a5f8103bf2724792b26ca400558437449c76b2
DeepakSinghN <dt7252058269@gmail.com>
2026-02-01 10:45:41 +0530

    Correctly type framer-motion variants

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,10 +2,10 @@
 
 import Link from 'next/link';
 import Image from 'next/image';
-import { motion } from 'framer-motion';
+import { motion, Variants } from 'framer-motion';
 
 const Footer = () => {
-    const containerVariants = {
+    const containerVariants: Variants = {
         hidden: { opacity: 0 },
         visible: {
             opacity: 1,
@@ -16,12 +16,12 @@ const Footer = () => {
         }
     };
 
-    const itemVariants = {
+    const itemVariants: Variants = {
         hidden: { opacity: 0, y: 30 },
         visible: {
             opacity: 1,
             y: 0,
-            transition: { duration: 0.8, ease: [0.22, 1, 0.36, 1] as [number, number, number, number] }
+            transition: { duration: 0.8, ease: [0.22, 1, 0.36, 1] }
         }
     };
 
